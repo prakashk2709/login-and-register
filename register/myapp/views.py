3daae05e5ad17c112c70e77199139b4d37e2ccb1
prakashk2709 <93462825+prakashk2709@users.noreply.github.com>
2021-11-01 15:29:55 +0530

    first commit

---
+++ register/myapp/views.py
@@ -0,0 +1,60 @@
+
+from django.shortcuts import render,redirect
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+from .models import Feature
+
+# Create your views here.
+
+
+
+def register(request):
+ if request.method == 'POST':
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+
+    if password2 == password2:
+        if User.objects.filter(email=email).exists():
+            messages.info(request, 'Email Already Used')
+            return redirect('register')
+        elif User.objects.filter(username=username).exists():
+                messages.info(request, 'username Already Used')
+                return redirect('register')
+        else:
+            user = User.objects.Create_user(username=username,email=email,password=password,password2=password2)        
+            user.save();
+            return redirect('login.html')
+    else:
+            messages.info(request,'Password Not the Same')
+            return redirect('register')
+
+ else:
+    return render(request,'register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+
+        else:
+            messages.info(request,'Credentials Invalid')
+            return redirect('login')
+    else:
+        return render(request,'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+    
+
+    
